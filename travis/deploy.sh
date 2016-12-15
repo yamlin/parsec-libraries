45e4925c2d45ff949a216148e22c9fb993080d6f
Yam Lin <yamlin@yahoo-inc.com>
2016-12-15 15:34:12 +0800

    fix

--- travis/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -ev
-
-test "${TRAVIS_PULL_REQUEST}" == "false"
-test "${TRAVIS_BRANCH}" == "master"
-
-./gradlew  bintrayUpload -PbintrayUser=${env.BINTRAY_USER} -PbintrayKey=${env.BINTRAY_API_KEY}
